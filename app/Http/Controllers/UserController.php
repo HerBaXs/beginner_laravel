743b03d45243fa5162a896d35f39de242556ded0
unknown <ern.herbaxs@gmail.com>
2023-08-09 01:40:45 +0300

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 
     public function updateAll(Request $request)
     {
-        dd($request->all());asdasdasd
+        dd($request->all());
     }
 
     public function users(Request $request)
